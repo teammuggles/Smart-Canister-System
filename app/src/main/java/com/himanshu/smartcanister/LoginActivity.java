c90137ebf081d06ff965277c3a8f74f05c82c57f
himanshu-gupta1 <creative.himanshu1@gmail.com>
2017-08-11 17:14:30 +0530

    initial

---
+++ app/src/main/java/com/himanshu/smartcanister/LoginActivity.java
@@ -0,0 +1,121 @@
+package com.himanshu.smartcanister;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+    GoogleApiClient mGoogleApiClient;
+    SignInButton signInButton;
+    private FirebaseAuth mAuth;
+    private static final int RC_SIGN_IN=1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth=FirebaseAuth.getInstance();
+        signInButton= (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(this);
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.sign_in_button)
+        {
+            signIn();
+        }
+    }
+
+    public void signIn()
+    {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    public void handleSignInResult(GoogleSignInResult result)
+    {
+        if(result.isSuccess())
+        {
+            GoogleSignInAccount account=result.getSignInAccount();
+            firebaseAuthWithGoogle(account);
+
+
+        }
+    }
+
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct)
+    {
+        AuthCredential credential=GoogleAuthProvider.getCredential(acct.getIdToken(),null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    finish();
+                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this,"Authentication Failed",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser=mAuth.getCurrentUser();
+        if(currentUser!=null)
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+    }
+
+
+}
---
+++ app/src/main/java/com/himanshu/smartcanister/MyFirebaseInstanceIDService.java
@@ -0,0 +1,9 @@
+package com.himanshu.smartcanister;
+
+/**
+ * Created by Himanshu Gupta on 8/11/2017.
+ */
+
+public class MyFirebaseInstanceIDService {
+
+}
